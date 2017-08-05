4ea0196543da2a8cbd9f424a41649e0dbe653345
James Falade <james.falade@konga.com>
2017-08-05 18:30:37 +0100

    Initial Commit

--- app/src/main/java/com/faladinojames/popularmovies/Constants.java
+++ app/src/main/java/com/faladinojames/popularmovies/Constants.java
@@ -8,5 +8,5 @@ public class Constants {
 
     public final static String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
     public final static String IMAGE_SMALL_SIZE = "w185";
-    public final static String API_KEY="ce96e400b466b9caa393be6e7b6b1e2f";
+    public final static String API_KEY="";
 }
